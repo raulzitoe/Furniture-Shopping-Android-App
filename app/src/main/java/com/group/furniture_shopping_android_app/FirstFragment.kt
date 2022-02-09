04d45a9d8393a58b77f5b2fe93d9431ed68ef37c
raulzitoe <raulguil@gmail.com>
2022-02-08 23:21:57 -0500

    created navigation component and first two screens layout

---
+++ app/src/main/java/com/group/furniture_shopping_android_app/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.group.furniture_shopping_android_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.google.android.material.navigation.NavigationBarItemView
+import com.group.furniture_shopping_android_app.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentFirstBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGetStarted.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
