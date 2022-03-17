55ecfda6dd1e4158f10d770e5b208fd99f7241d1
raulzitoe <raulguil@gmail.com>
2022-03-17 15:58:42 -0400

    improved product layout

--- app/src/main/java/com/group/furniture_shopping_android_app/product_details/ProductDetailsFragment.kt
+++ app/src/main/java/com/group/furniture_shopping_android_app/product_details/ProductDetailsFragment.kt
@@ -7,10 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.group.furniture_shopping_android_app.MainActivity
+import com.group.furniture_shopping_android_app.R
 import com.group.furniture_shopping_android_app.databinding.FragmentProductBinding
 
 class ProductDetailsFragment : Fragment() {
@@ -35,11 +37,16 @@ class ProductDetailsFragment : Fragment() {
         val viewStateObserver = Observer<ProductDetailsViewState> { viewState ->
             when(viewState){
                 is ProductDetailsViewState.Success -> {
+                    val reviewsCount = viewState.data.reviewsCount
                     with(binding){
                         fragmentProductName.text = viewState.data.name
                         fragmentProductPrice.text = viewState.data.price
                         fragmentProductRating.text = viewState.data.rating.toString()
-                        fragmentProductReviewsCount.text = viewState.data.reviewsCount.toString()
+
+                        fragmentProductReviewsCount.text = if (reviewsCount == 0) {
+                            resources.getString(R.string.product_info_quantity_reviews_zero, reviewsCount)
+                        }
+                        else resources.getQuantityString(R.plurals.product_info_quantity_reviews, reviewsCount, reviewsCount)
                         fragmentProductDescription.text = viewState.data.description
                     }
                     val bitmap = BitmapFactory.decodeFile(viewState.data.localImagePath)
@@ -49,9 +56,11 @@ class ProductDetailsFragment : Fragment() {
             }
         }
         viewModel.viewState.observe(viewLifecycleOwner, viewStateObserver)
+        binding.btnBack.setOnClickListener {
+            activity?.onBackPressed()
+        }
 
     }
-
     override fun onDestroy() {
         super.onDestroy()
         (activity as MainActivity).setBottomNavigationVisibility(View.VISIBLE)
