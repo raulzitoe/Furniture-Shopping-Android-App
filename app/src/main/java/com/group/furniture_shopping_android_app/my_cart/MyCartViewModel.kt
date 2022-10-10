654fb6b5f416a4d6668f801773d4bf181a479aec
Raul Vieira <raulguil@gmail.com>
2022-10-10 18:17:31 -0400

    Remove unused imports

--- app/src/main/java/com/group/furniture_shopping_android_app/my_cart/MyCartViewModel.kt
+++ app/src/main/java/com/group/furniture_shopping_android_app/my_cart/MyCartViewModel.kt
@@ -4,9 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.gson.Gson
-import com.group.furniture_shopping_android_app.ProductListModel
-import com.group.furniture_shopping_android_app.R
 import com.group.furniture_shopping_android_app.repository.AppRepository
 import com.group.furniture_shopping_android_app.repository.CartModel
 import dagger.hilt.android.lifecycle.HiltViewModel
