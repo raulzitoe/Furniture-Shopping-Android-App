2c05507355335c8107435739a9535e439d7be137
Raul Vieira <raulguil@gmail.com>
2022-10-27 17:12:49 -0400

    Reorganize AppDao.kt

--- app/src/main/java/com/group/furniture_shopping_android_app/repository/AppDao.kt
+++ app/src/main/java/com/group/furniture_shopping_android_app/repository/AppDao.kt
@@ -6,58 +6,63 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
 
 
 @Dao
 interface AppDao {
 
+    //Cart Operations
     @Insert
     suspend fun insertToCart(cartItem: CartModel)
-
-    @Insert
-    suspend fun insertToOrders(orderItem: OrderModel)
-
-    @Insert
-    suspend fun insertToFavorites(favoritesItem: FavoritesModel)
-
-    @Insert
-    suspend fun insertToShippingAddresses(shippingItem: ShippingAddressModel)
-
-    @Query("Select * from cart_table")
-    fun getAllCartItems(): Flow<List<CartModel>>
-
-    @Query("Select * from order_table")
-    fun getAllOrderItems(): Flow<List<OrderModel>>
-
-    @Query("Select * from favorites_table")
-    fun getAllFavoritesItems(): Flow<List<FavoritesModel>>
-
-    @Query("Select * from shipping_addresses_table")
-    fun getAllShippingItems(): Flow<List<ShippingAddressModel>>
 
     @Delete
     suspend fun removeFromCart(cartItem: CartModel)
 
+    @Update
+    suspend fun updateItemFromCart(cartItem: CartModel)
+
+    @Query("Select * from cart_table")
+    fun getAllCartItems(): Flow<List<CartModel>>
+
+
+    //Orders Operations
+    @Insert
+    suspend fun insertToOrders(orderItem: OrderModel)
+
     @Delete
     suspend fun removeFromOrders(orderItem: OrderModel)
 
-    @Delete
-    suspend fun removeFromFavorites(favoritesItem: FavoritesModel)
+    @Query("Select * from order_table")
+    fun getAllOrderItems(): Flow<List<OrderModel>>
+
+    @Query("Select COUNT(id) FROM order_table")
+    fun getOrdersQuantity(): Flow<Int>
+
+
+    //Shipping Addresses Operations
+    @Insert
+    suspend fun insertToShippingAddresses(shippingItem: ShippingAddressModel)
 
     @Delete
     suspend fun removeFromShipping(shippingItem: ShippingAddressModel)
 
     @Update
-    suspend fun updateItemFromCart(cartItem: CartModel)
-
-    @Update
     suspend fun updateShippingAddress(shippingItem: ShippingAddressModel)
+
+    @Query("Select * from shipping_addresses_table")
+    fun getAllShippingItems(): Flow<List<ShippingAddressModel>>
 
     @Query("Select COUNT(id) FROM shipping_addresses_table")
     fun getShippingAddressesQuantity(): Flow<Int>
 
-    @Query("Select COUNT(id) FROM order_table")
-    fun getOrdersQuantity(): Flow<Int>
 
+    //Favorites Operations
+    @Insert
+    suspend fun insertToFavorites(favoritesItem: FavoritesModel)
+
+    @Delete
+    suspend fun removeFromFavorites(favoritesItem: FavoritesModel)
+
+    @Query("Select * from favorites_table")
+    fun getAllFavoritesItems(): Flow<List<FavoritesModel>>
 }
