0dd8304dae9e9ba118686c49bc6679e5fe879da6
Raul Vieira <raulguil@gmail.com>
2022-10-27 19:38:38 -0400

    Remove unused MyCartListModel.kt

--- app/src/main/java/com/group/furniture_shopping_android_app/my_cart/MyCartListModel.kt
+++
@@ -1,7 +0,0 @@
-package com.group.furniture_shopping_android_app.my_cart
-
-import com.group.furniture_shopping_android_app.ProductModel
-
-data class MyCartListModel (
-    var myCartList: ArrayList<ProductModel> = arrayListOf()
-)
