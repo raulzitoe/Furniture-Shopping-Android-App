00d824d3c7990215df69aa38fe294aed11e07e5e
Raul Vieira <raulguil@gmail.com>
2022-11-02 16:38:38 -0400

    Implement Firebase authentication

--- app/src/main/java/com/group/furniture_shopping_android_app/login_signup/LoginFragment.kt
+++ app/src/main/java/com/group/furniture_shopping_android_app/login_signup/LoginFragment.kt
@@ -1,18 +1,24 @@
 package com.group.furniture_shopping_android_app.login_signup
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.group.furniture_shopping_android_app.R
 import com.group.furniture_shopping_android_app.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +29,49 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_loginFragment_to_homeActivity)
+        }
+
         binding.tvSignup.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_signUpFragment)
         }
         binding.btnLogin.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_homeActivity)
+            var email: String = binding.etEmail.text.toString()
+            var password: String = binding.etPassword.text.toString()
+
+            //Auth from firebase requires non-empty string
+            if (email.isEmpty()) email = "-"
+            if (password.isEmpty()) password = "-"
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Login", "signInWithEmail:success")
+                        Navigation.findNavController(view)
+                            .navigate(R.id.action_loginFragment_to_homeActivity)
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Login", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            context, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
     }
 }
